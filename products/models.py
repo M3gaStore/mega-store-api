9e4f3e1d01550f800a0c6b171a070b59d2b6de2a
CRomero19 <ronerodocarmojunior@hotmail.com>
2023-07-03 17:39:57 -0300

    fix:minor-change-product-model

--- products/models.py
+++ products/models.py
@@ -7,7 +7,8 @@ class Product(models.Model):
     description = models.TextField()
     value = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=50)
-    quantity_in_stock = models.IntegerField()
+    quantity_in_stock = models.PositiveIntegerField()
+    is_available = models.BooleanField(default=True)
 
     owner = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="products"
