3db643b05c0e073c8e8e35f16d9fe253d219d894
CRomero19 <ronerodocarmojunior@hotmail.com>
2023-07-03 14:50:48 -0300

    refactor:models-serializers/feat:create-user

--- addresses/models.py
+++ addresses/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Address(models.Model):
-    name = models.CharField(max_length=50)
+    street = models.CharField(max_length=150)
+    state = models.CharField(max_length=30)
+    number = models.IntegerField()
+    neighborhood = models.CharField(max_length=60)
+    zip_code = models.IntegerField()
--- orders/models.py
+++ orders/models.py
@@ -10,14 +10,23 @@ class OrderStatusChoices(models.TextChoices):
 
 class Order(models.Model):
     user = models.ForeignKey(
-        "users.User", on_delete=models.PROTECT, related_name="user"
-    )
-    product = models.ForeignKey(
-        "products.Product", on_delete=models.PROTECT, related_name="product"
+        "users.User", on_delete=models.PROTECT, related_name="orders"
     )
     status = models.CharField(
         max_length=20,
         choices=OrderStatusChoices.choices,
         default=OrderStatusChoices.EM_ANDAMENTO,
     )
+
+    total_value = models.FloatField()
+
     created_at = models.DateTimeField(auto_now_add=True)
+
+    product_order = models.ManyToManyField("products.Product", through="orders.OrderItem", related_name="order_products")
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey("orders.Order", on_delete=models.CASCADE)
+    product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
+
+    quantity = models.IntegerField()
