6dafb33d89425f5a0a4d54cf8bddf12a505ac6dd
CRomero19 <ronerodocarmojunior@hotmail.com>
2023-07-04 14:43:14 -0300

    feat:filter-get-products

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,6 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path("products/", views.ProductsView.as_view()),
+    path("products/<int:product_id>/", views.SingleProductView.as_view()),
+    path("user/<int:user_id>/products/", views.SingleProductView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -2,15 +2,20 @@ from rest_framework import generics
 from django.shortcuts import get_object_or_404
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from products.filters import ProductsFilter
 
 from products.models import Product
 from products.serializers import ProductSerializer
 from users.models import User
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ProductsView(generics.ListCreateAPIView):
+    filter_backends = [DjangoFilterBackend]
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filterset_class = ProductsFilter
 
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -19,7 +24,6 @@ class ProductsView(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
     def get(self, request, *args, **kwargs):
-        print(request.data)
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
